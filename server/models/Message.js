c4ca157bbf93e0bb98a0f9a8e1aacf1d0fd70623
Lohita Venkatesh <lohitavenkatesh@gmail.com>
2026-01-07 09:53:13 +0530

    create model

---
+++ server/models/Message.js
@@ -0,0 +1,12 @@
+import mongoose from "mongoose";
+
+const messageSchema = new mongoose.Schema({
+    userId : {type:Number, required: true},
+    userName : {type:String, required: true},
+    messageText : {type:String, required: true},
+    urgency : {type: String, enum:["high","medium","low"]},
+    status: {type:String, enum:["replied","open"], default:"open"},
+    agentReply: String,
+    createdAt: {type:Date, default:Date.now},
+    repliedAt: Date
+})
